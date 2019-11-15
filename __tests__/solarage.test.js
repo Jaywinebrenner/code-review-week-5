baee1588c16e0f193f3d7212077467daca066ef8
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-11-15 15:27:51 -0800

    refactor tests and successfully test refactored code

--- __tests__/solarage.test.js
+++ __tests__/solarage.test.js
@@ -1,72 +1,36 @@
 import { mercuryExpectancy, mercuryAge, venusAge, marsAge, jupiterAge, saturnAge, uranusAge, neptuneAge, PlanetExpectancy } from './../src/solarage.js';
 
+describe('Solar Age planet testing', () => {
 
-// describe('Solar Age planet testing', () => {
-//
-//   test('should correctly return the inputted Earth age in Mercury Age', () => {
-//     let userMercuryAge = mercuryAge(40);
-//     expect(userMercuryAge).toEqual(166.7);
-//   });
-//   test('should correctly return the inputted Earth age in Venus Age', () => {
-//     let userVenusAge = venusAge(40);
-//     expect(userVenusAge).toEqual(64.5);
-//   });
-//   test('should correctly return the inputted Earth age in Mars Age', () => {
-//     let userMarsAge = marsAge(40);
-//     expect(userMarsAge).toEqual(21.3);
-//   });
-//   test('should correctly return the inputted Earth age in Jupiter Age', () => {
-//      let userJupiterAge = jupiterAge(40);
-//     expect(userJupiterAge).toEqual(3.4);
-//   });
-//   test('should correctly return the inputted Earth age in Saturn Age', () => {
-//     let userSaturnAge = saturnAge(40);
-//     expect(userSaturnAge).toEqual(1.4);
-//   });
-//   test('should correctly return the inputted Earth age in Uranus Age', () => {
-//     let userUranusAge = uranusAge(40);
-//     expect(userUranusAge).toEqual(.48);
-//   });
-//   test('should correctly return the inputted Earth age in Neptune Age', () => {
-//     let userNeptuneAge = neptuneAge(40);
-//     expect(userNeptuneAge).toEqual(.24);
-//   });
-// });
-//
-//
-// describe('Determines how many solar years one has left to live', () => {
-//
-//   test('should correctly return how many Mercury years one has left to live', () => {
-//     let mercuryYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(mercuryYearsLeft.calculateExpectancyMercury()).toEqual(225);
-//   });
-//   test('should correctly return how many Venus years one has left to live', () => {
-//     let venusYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(venusYearsLeft.calculateExpectancyVenus()).toEqual(87.1);
-//   });
-//   test('should correctly return how many Mars years one has left to live', () => {
-//     let marsYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(marsYearsLeft.calculateExpectancyMars()).toEqual(28.7);
-//   });
-//   test('should correctly return how many Jupiter years one has left to live', () => {
-//     let jupiterYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(jupiterYearsLeft.calculateExpectancyJupiter()).toEqual(4.6);
-//   });
-//   test('should correctly return how many Jupiter years one has left to live', () => {
-//     let saturnYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(saturnYearsLeft.calculateExpectancySaturn()).toEqual(1.9);
-//   });
-//   test('should correctly return how many Uranus years one has left to live', () => {
-//     let uranusYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(uranusYearsLeft.calculateExpectancyUranus()).toEqual(.64);
-//   });
-//   test('should correctly return how many Neptune years one has left to live', () => {
-//     let neptuneYearsLeft = new PlanetExpectancy(74, 20);
-//     expect(neptuneYearsLeft.calculateExpectancyNeptune()).toEqual(.33);
-//   });
-//
-// });
-
+  test('should correctly return the inputted Earth age in Mercury Age', () => {
+    let userMercuryAge = mercuryAge(40);
+    expect(userMercuryAge).toEqual(166.7);
+  });
+  test('should correctly return the inputted Earth age in Venus Age', () => {
+    let userVenusAge = venusAge(40);
+    expect(userVenusAge).toEqual(64.5);
+  });
+  test('should correctly return the inputted Earth age in Mars Age', () => {
+    let userMarsAge = marsAge(40);
+    expect(userMarsAge).toEqual(21.3);
+  });
+  test('should correctly return the inputted Earth age in Jupiter Age', () => {
+     let userJupiterAge = jupiterAge(40);
+    expect(userJupiterAge).toEqual(3.4);
+  });
+  test('should correctly return the inputted Earth age in Saturn Age', () => {
+    let userSaturnAge = saturnAge(40);
+    expect(userSaturnAge).toEqual(1.4);
+  });
+  test('should correctly return the inputted Earth age in Uranus Age', () => {
+    let userUranusAge = uranusAge(40);
+    expect(userUranusAge).toEqual(.48);
+  });
+  test('should correctly return the inputted Earth age in Neptune Age', () => {
+    let userNeptuneAge = neptuneAge(40);
+    expect(userNeptuneAge).toEqual(.24);
+  });
+});
 
 
 describe('Determines how many solar years one has left to live and returns amount of solar years already surpassing life expectancy if so', () => {
--- src/solarage.js
+++ src/solarage.js
@@ -1,76 +1,38 @@
-//
-//
-// export function mercuryAge(earthAge){
-//   let preRound = earthAge / .24;
-//   return Math.round(preRound * 10) / 10;
-// }
-//
-// export function venusAge(earthAge){
-//   let preRound = earthAge / .62;
-//   return Math.round(preRound * 10) / 10;
-// }
-//
-// export function marsAge(earthAge){
-//   let preRound = earthAge / 1.88 ;
-//   return Math.round(preRound * 10) / 10;
-// }
-//
-// export function jupiterAge(earthAge){
-//   let preRound = earthAge / 11.86 ;
-//   return Math.round(preRound * 10) / 10;
-// }
-//
-// export function saturnAge(earthAge){
-//   let preRound = earthAge / 29 ;
-//   return Math.round(preRound * 10) / 10;
-// }
-//
-// export function uranusAge(earthAge){
-//   let preRound = earthAge / 84 ;
-//   return Math.round(preRound * 100) / 100;
-// }
-// export function neptuneAge(earthAge){
-//   let preRound = earthAge / 165 ;
-//   return Math.round(preRound * 100) / 100;
-// }
-//
-//
-//
-// export class PlanetExpectancy {
-//   constructor(lifeExpectancy, earthAge) {
-//     this.lifeExpectancy = lifeExpectancy;
-//     this.earthAge = earthAge;
-//   }
-//   calculateExpectancyMercury(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / .24;
-//     return Math.round(earthYearsLeft * 10) / 10;
-//   }
-//   calculateExpectancyVenus(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / .62;
-//     return Math.round(earthYearsLeft * 10) / 10;
-//   }
-//   calculateExpectancyMars(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / 1.88;
-//     return Math.round(earthYearsLeft * 10) / 10;
-//   }
-//   calculateExpectancyJupiter(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / 11.86;
-//     return Math.round(earthYearsLeft * 10) / 10;
-//   }
-//   calculateExpectancySaturn(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / 29;
-//     return Math.round(earthYearsLeft * 10) / 10;
-//   }
-//   calculateExpectancyUranus(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / 84;
-//     return Math.round(earthYearsLeft * 100) / 100;
-//   }
-//   calculateExpectancyNeptune(){
-//     let earthYearsLeft = (this.lifeExpectancy - this.earthAge) / 165;
-//     return Math.round(earthYearsLeft * 100) / 100;
-//   }
-//
-// }
+export function mercuryAge(earthAge){
+  let preRound = earthAge / .24;
+  return Math.round(preRound * 10) / 10;
+}
+
+export function venusAge(earthAge){
+  let preRound = earthAge / .62;
+  return Math.round(preRound * 10) / 10;
+}
+
+export function marsAge(earthAge){
+  let preRound = earthAge / 1.88 ;
+  return Math.round(preRound * 10) / 10;
+}
+
+export function jupiterAge(earthAge){
+  let preRound = earthAge / 11.86 ;
+  return Math.round(preRound * 10) / 10;
+}
+
+export function saturnAge(earthAge){
+  let preRound = earthAge / 29 ;
+  return Math.round(preRound * 10) / 10;
+}
+
+export function uranusAge(earthAge){
+  let preRound = earthAge / 84 ;
+  return Math.round(preRound * 100) / 100;
+}
+export function neptuneAge(earthAge){
+  let preRound = earthAge / 165 ;
+  return Math.round(preRound * 100) / 100;
+}
+
+
 
 export class PlanetExpectancy {
   constructor(lifeExpectancy, earthAge) {
