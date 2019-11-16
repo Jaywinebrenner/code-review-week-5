df7c3b91b429a10d2e406e433fba2b0edda55ef1
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-11-15 16:38:43 -0800

    Add html for Jupiter, Saturn, Uranus. Import google font for h1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-// this will push a clean commit message for this file because I totally botched my first commit. I will go back and figure out how to edit commit messages if I have time. But as I look at my commit messages, I cringe. So this should overwrite the commit message for this file.
-
-
-
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { mercuryExpectancy, mercuryAge, venusAge, marsAge, jupiterAge, saturnAge, uranusAge, neptuneAge, PlanetExpectancy } from './../src/solarage.js';
+
+$(document).ready(function(){
+  $("#marsForm").submit(function(event){
+    event.preventDefault();
+    let earthAgeInput = $("mercuryInput").val();
+    let mercuryAgeOutput = mercuryAge(earthAgeInput);
+    $("#mercurySpan").text(mercuryAgeOutput);
+
+
+  });
+});
